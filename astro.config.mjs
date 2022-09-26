c49d34a130607270d070300ed76212be435d8aee
galifornia <martinho87@gmail.com>
2022-09-26 23:40:22 +0200

    Add prettier config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [sitemap()],
-});
+  site: 'https://example.com',
+  integrations: [sitemap()],
+})
