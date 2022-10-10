c29995686081562a3d9dc2f3f2a5513715ac5b0c
galifornia <martinho87@gmail.com>
2022-10-10 12:54:57 +0200

    Update website information

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'What I Use - martingl';
+export const SITE_DESCRIPTION = 'A curated list of something cool';
+export const HOMEPAGE_URL = 'https://martingl.dev'
